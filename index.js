5e3f9cdc652699fa3b3e3307bc3bba05c569071b
Philip Samoei <samphilkerich04@gmail.com>
2023-03-11 17:34:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+console.log(newPosition);
+
+  function moveDodgerRight() {
+    const leftPosition = dodger.style.left.replace("px", "");
+    const left= parseInt(leftPosition, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerRight();
+    }
+  });
+  console.log(newPosition);
